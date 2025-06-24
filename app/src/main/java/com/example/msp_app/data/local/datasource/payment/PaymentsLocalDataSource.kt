7fab3b087201a66fa1d66b14f447e5e5542a639c
Abdiel <abdimuy@gmail.com>
2025-06-24 15:18:37 -0600

    Add insertPaymentAndUpdateSale function to PaymentsLocalDataSource for improved transaction handling

--- app/src/main/java/com/example/msp_app/data/local/datasource/payment/PaymentsLocalDataSource.kt
+++ app/src/main/java/com/example/msp_app/data/local/datasource/payment/PaymentsLocalDataSource.kt
@@ -1,11 +1,14 @@
 package com.example.msp_app.data.local.datasource.payment;
 
 import android.content.Context
+import androidx.room.Transaction
 import com.example.msp_app.data.local.AppDatabase
 import com.example.msp_app.data.local.entities.PaymentEntity
+import com.example.msp_app.data.models.sale.EstadoCobranza
 
 class PaymentsLocalDataSource(context: Context) {
     private val paymentDao = AppDatabase.getInstance(context).paymentDao()
+    private val saleDao = AppDatabase.getInstance(context).saleDao()
 
     suspend fun getPaymentById(id: String): PaymentEntity {
         return paymentDao.getPaymentById(id)
@@ -22,5 +25,16 @@ class PaymentsLocalDataSource(context: Context) {
     suspend fun saveAll(payments: List<PaymentEntity>) {
         paymentDao.deleteAll()
         paymentDao.saveAll(payments)
+    }
+
+    @Transaction
+    suspend fun insertPaymentAndUpdateSale(
+        payment: PaymentEntity,
+        saleId: Int,
+        newAmount: Double,
+        newEstadoCobranza: EstadoCobranza
+    ) {
+        paymentDao.savePayment(payment)
+        saleDao.updateTotal(saleId, newAmount, newEstadoCobranza)
     }
 }
