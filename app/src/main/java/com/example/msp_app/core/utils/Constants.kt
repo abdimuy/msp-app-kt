312cc0ea992e4a2e538923cd1dfdc6b07f5b3c75
Abdiel <abdimuy@gmail.com>
2025-06-25 14:15:58 -0600

    Add USERS_COLLECTION constant for Firebase Firestore integration

--- app/src/main/java/com/example/msp_app/core/utils/Constants.kt
+++ app/src/main/java/com/example/msp_app/core/utils/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     const val PAGO_EN_EFECTIVO_ID = 157
     const val PAGO_CON_TRANSFERENCIA_ID = 52569
     const val CONDONACION_ID = 137026
+
+    // Firebase Firestore collection names
+    const val USERS_COLLECTION = "users"
 }
