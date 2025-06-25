bbdd87c60d11b2ed840152cd93513e6708eedc66
Abdiel <abdimuy@gmail.com>
2025-06-25 13:09:25 -0600

    Refactor AuthViewModel to remove unused imports and comments for cleaner code

--- app/src/main/java/com/example/msp_app/features/auth/viewModels/AuthViewModel.kt
+++ app/src/main/java/com/example/msp_app/features/auth/viewModels/AuthViewModel.kt
@@ -1,23 +1,19 @@
 package com.example.msp_app.features.auth.viewModels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class AuthViewModel : ViewModel() {
 
     private val auth = FirebaseAuth.getInstance()
 
-    // Usuario actual como flujo reactivo
     private val _currentUser = MutableStateFlow<FirebaseUser?>(auth.currentUser)
     val currentUser: StateFlow<FirebaseUser?> = _currentUser
 
     init {
-        // Listener que actualiza cuando el usuario cambia
         auth.addAuthStateListener {
             _currentUser.value = it.currentUser
         }
