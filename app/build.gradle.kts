0f0a35a1385a5d277af373f34db3ebdea330e606
Abdiel <abdimuy@gmail.com>
2025-07-25 13:00:49 -0600

    Update app version to 2.0.4
    The `versionCode` in `app/build.gradle.kts` has been incremented from 3 to 4.
    The `versionName` in `app/build.gradle.kts` has been updated from "2.0.3" to "2.0.4".
    The `APP_VERSION` constant in `app/src/main/java/com/example/msp_app/core/utils/Constants.kt` has been updated from "2.0.3" to "2.0.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.msp_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.3"
+        versionCode = 4
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = localProps.getProperty("MAPS_API_KEY", "")
