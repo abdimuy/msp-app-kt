cc8c9b82f2a4c8d7efe73c2257a7e633182b68d5
Abdiel <abdimuy@gmail.com>
2025-07-28 13:18:44 -0600

    Update ESCPOS-ThermalPrinter-Android library to version 3.4.0
    The ESCPOS-ThermalPrinter-Android library has been updated from version 2.0.6 to 3.4.0 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
 
     implementation("androidx.work:work-runtime-ktx:2.10.2")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
-    implementation("com.github.DantSu:ESCPOS-ThermalPrinter-Android:2.0.6")
+    implementation("com.github.DantSu:ESCPOS-ThermalPrinter-Android:3.4.0")
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
