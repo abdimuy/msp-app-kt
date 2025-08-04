e0557a19f2af128335dcc106df9039d33ac36b29
cerza <cirojulian777@gmail.com>
2025-08-04 14:15:48 -0600

    Add confirmation dialog to download update, conditional on active session

--- app/src/main/java/com/example/msp_app/MainActivity.kt
+++ app/src/main/java/com/example/msp_app/MainActivity.kt
@@ -8,10 +8,22 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.lifecycleScope
 import com.example.msp_app.core.utils.downloadApk
 import com.example.msp_app.core.utils.installApk
+import com.example.msp_app.data.api.ApiProvider
+import com.example.msp_app.features.auth.viewModels.AuthViewModel
 import com.example.msp_app.navigation.AppNavigation
 import com.example.msp_app.ui.theme.MspappTheme
 import com.example.msp_app.ui.theme.ThemeController
@@ -21,11 +33,11 @@ import com.google.firebase.firestore.persistentCacheSettings
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val authViewModel: AuthViewModel by viewModels()
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //val currentVersionCode = getCurrentVersionCode(this)
-        //val currentVersionName = packageManager.getPackageInfo(packageName, 0).versionName
 
         val settings = FirebaseFirestoreSettings.Builder()
             .setLocalCacheSettings(
@@ -34,7 +46,6 @@ class MainActivity : ComponentActivity() {
                 }
             )
             .build()
-
         FirebaseFirestore.getInstance().firestoreSettings = settings
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -50,88 +61,63 @@ class MainActivity : ComponentActivity() {
         ThemeController.init(this)
         enableEdgeToEdge()
 
-        lifecycleScope.launch {
-            try {
-                val apkUrl =
-                    "https://msp2025.loclx.io/download-app/app-release.apk"
-                val apkFile = downloadApk(this@MainActivity, apkUrl)
-
-                Toast.makeText(
-                    this@MainActivity,
-                    "Descarga completada. Abriendo instalador...",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                installApk(this@MainActivity, apkFile)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(
-                    this@MainActivity,
-                    "Error al descargar o instalar el APK",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-
-
         setContent {
+            val currentUser by authViewModel.currentUser.collectAsState()
+            var showDialog by remember { mutableStateOf(false) }
+            var downloadStarted by remember { mutableStateOf(false) }
+            val baseUrl by ApiProvider.baseURL.collectAsState()
+
             MspappTheme(dynamicColor = false) {
                 AppNavigation()
+
+                if (currentUser != null && !downloadStarted && showDialog) {
+                    AlertDialog(
+                        onDismissRequest = { showDialog = false },
+                        title = { Text("Actualizar App") },
+                        text = { Text("¿Deseas descargar la actualizacion de la aplicación?") },
+                        confirmButton = {
+                            Button(onClick = {
+                                showDialog = false
+                                downloadStarted = true
+                                lifecycleScope.launch {
+                                    try {
+                                        val apkUrl =
+                                            "${baseUrl}download-app/app-release.apk"
+                                        val apkFile = downloadApk(this@MainActivity, apkUrl)
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Descarga completada. Abriendo instalador...",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        installApk(this@MainActivity, apkFile)
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Error al descargar o instalar el APK",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                }
+                            }) {
+                                Text("Sí")
+                            }
+                        },
+                        dismissButton = {
+                            Button(onClick = {
+                                showDialog = false
+                            }) {
+                                Text("No")
+                            }
+                        }
+                    )
+                }
+                LaunchedEffect(currentUser) {
+                    if (currentUser != null && !downloadStarted) {
+                        showDialog = true
+                    }
+                }
             }
         }
     }
-
-    /**private suspend fun fetchLatestVersionInfo(url: String): Pair<Int, String> =
-    withContext(Dispatchers.IO) {
-    val client = OkHttpClient()
-    val request = Request.Builder().url(url).build()
-    val response = client.newCall(request).execute()
-    val jsonString = response.body?.string() ?: ""
-
-    val json = JSONObject(jsonString)
-    val versionCode = json.getInt("versionCode")
-    val apkUrl = json.getString("apkUrl")
-
-    Pair(versionCode, apkUrl)
-    }**/
-
-    /**private fun showUpdateDialog(apkUrl: String) {
-    AlertDialog.Builder(this)
-    .setTitle("Nueva actualización disponible")
-    .setMessage("¿Deseas descargar e instalar la actualización?")
-    .setPositiveButton("Sí") { dialog, _ ->
-    lifecycleScope.launch {
-    try {
-    Toast.makeText(
-    this@MainActivity,
-    "Descargando actualización...",
-    Toast.LENGTH_SHORT
-    ).show()
-
-    val apkFile = downloadApk(this@MainActivity, apkUrl)
-
-    Toast.makeText(
-    this@MainActivity,
-    "Descarga completada. Abriendo instalador...",
-    Toast.LENGTH_SHORT
-    ).show()
-
-    installApk(this@MainActivity, apkFile)
-    } catch (e: Exception) {
-    e.printStackTrace()
-    Toast.makeText(
-    this@MainActivity,
-    "Error al descargar la actualización",
-    Toast.LENGTH_LONG
-    ).show()
-    }
-    }
-    dialog.dismiss()
-    }
-    .setNegativeButton("No") { dialog, _ ->
-    dialog.dismiss()
-    }
-    .setCancelable(false)
-    .show()
-    }**/
 }
