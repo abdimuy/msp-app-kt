2da7ea503530eb3646c77712a3a04c245afa7fde
Abdiel <abdimuy@gmail.com>
2025-10-02 11:34:32 -0600

    Update app version to 2.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.example.msp_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.2.1"
+        versionCode = 14
+        versionName = "2.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
