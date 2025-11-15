9eb5f31a1afbab72d705cf2832f8700c7c6976d2
Aldrich Cortero <abdidvp@gmail.com>
2025-11-15 12:28:39 -0600

    chore: Bump version to 2.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.example.msp_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.5.0"
+        versionCode = 21
+        versionName = "2.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
