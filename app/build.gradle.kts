aac30fc06a9d0b7f857a0b40d0baa78acd63bef6
Aldrich Cortero <abdidvp@gmail.com>
2026-01-09 14:53:12 -0600

    chore: Bump version to 2.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.example.msp_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 26
-        versionName = "2.7.0"
+        versionCode = 27
+        versionName = "2.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
